b99d48d56fa35ba4b2bcf19dfaa735c657f4695b
Lojaw <j.p.schmitz1@web.de>
2023-08-07 22:22:33 +0200

    test

--- main.cpp
+++ main.cpp
@@ -29,5 +29,7 @@ int main() {
     // Warte auf den Timer-Thread, um sicherzustellen, dass er abgeschlossen ist, bevor das Programm beendet wird
     timerThread.join();
 
+    //TEST
+
     return 0;
 }
